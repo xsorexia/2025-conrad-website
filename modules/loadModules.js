b56a9d17b49c4092d01054089ccecefe4abe3ebb
xsorexia <xsorexia@gmail.com>
2025-01-05 00:16:06 +0900

    new mission hehe

--- modules/loadModules.js
+++ modules/loadModules.js
@@ -14,3 +14,15 @@ function showContact() {
 function closeContact() {
     document.getElementById("contact").style.top = "-200px";
 }
+
+function toggleMission(element) {
+    if (element.style.transform == "rotate(45deg)") {
+        element.style.transform = "rotate(0)";
+        element.parentElement.parentElement.style.maxHeight = "25px";
+    } else {
+        element.style.transform = "rotate(45deg)";
+        element.parentElement.parentElement.style.maxHeight = "300px";
+
+    }
+
+}
